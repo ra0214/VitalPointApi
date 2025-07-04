681641355caa442cbc0581391feb47fdcfcf4170
christianramirez <cm2849436@gmail.com>
2025-07-04 02:47:37 -0600

    Agregué cors en el main

--- main.go
+++ main.go
@@ -13,6 +13,18 @@ import (
 func main() {
 	r := gin.Default()
 
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()	
+	})
+
 	// Inicializar repositorios MYSQL
 	userRepo := userInfra.NewMySQL()
 	maxRepo := maxInfra.NewMySQL()
